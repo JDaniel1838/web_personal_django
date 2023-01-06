f2ed3ed64ab928f46da56bb2856dade2bf8b1867
jdaniel1838 <lx15787@innovaccion.mx>
2023-01-06 16:13:49 -0600

    laboratorio 2

--- web_personal/core/views.py
+++ web_personal/core/views.py
@@ -1,47 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
 
-# var menu with code HTML.
-html_base = """
- <h1>Mi Web Personal</h1>
- <ul>
- <li><a href="/">Portada</a></li>
- <li><a href="/about/">Acerca de</a></li>
- <li><a href="/portfolio/">Portafolio</a></li>
- <li><a href="/contact/">Contacto</a></li>
- </ul>
-"""
-
 # view home
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Bienvenidos</h2>
-        <p>Esto es la portada.</p>
-    """)
+    return render(request,'core/home.html')
 
 # view about
 def about(request):
-    return HttpResponse("""
-    <h1>Mi Web Personal</h1>
-    <h2>Acerca de</h2>
-    <p>Me llamo Daniel y me encanta Django!</p>
-    """)
+    return render(request,'core/about.html')
 
 # view portfolio
 def portfolio(request):
-    return HttpResponse("""
-    <h2>Mi portafolio</h2>
-    <p>Aquí encontraras todos mis proyectos</p>
-    """)
+    return render(request,'core/portfolio.html')
 
 # view contact
 def contact(request):
-    return HttpResponse("""
-    <h2>Contacto</h2>
-    <p>Aquí encontraras mi email y redes sociales para contactarme</p>
-    <ul>
-        <li><a href='www.google.com'>Email</a></li>
-        <li><a href='www.github.com'>Github</a></li>
-        <li><a href='www.youtube.com'>Youtube</a></li>
-    </ul>
-    """)
+    return render(request,'core/contact.html')
--- web_personal/web_personal/settings.py
+++ web_personal/web_personal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'#templates from core
 ]
 
 MIDDLEWARE = [
