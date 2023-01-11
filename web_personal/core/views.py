afe46663c89347faa3d6d7fd53f535906fc5eff5
jdaniel1838 <lx15787@innovaccion.mx>
2023-01-11 17:28:05 -0600

    laboratorio 3

--- web_personal/core/views.py
+++ web_personal/core/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render, HttpResponse
 
 # view home
 def home(request):
-    return render(request,'core/home.html')
+    return render(request,'core/home.html',{'title': "Portada | Mi Web Personal"})
 
 # view about
 def about(request):
-    return render(request,'core/about.html')
+    return render(request,'core/about.html',{'title': "Acerca de | Mi Web Personal"})
 
 # view portfolio
 def portfolio(request):
-    return render(request,'core/portfolio.html')
+    return render(request,'core/portfolio.html',{'title': "Portafolio | Mi Web Personal"})
 
 # view contact
 def contact(request):
-    return render(request,'core/contact.html')
+    return render(request,'core/contact.html',{'title': "Contacto | Mi Web Personal"})
